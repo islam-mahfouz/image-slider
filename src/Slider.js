fa6f11cf65716e30022294a579ed263443e6bf18
islam <islamheshamashraf@gmail.com>
2016-10-25 02:54:00 +0300

    changes images

--- src/Slider.js
+++ src/Slider.js
@@ -6,7 +6,7 @@ export default class Slider extends React.Component {
  constructor() {
   super()
   this.state = {
-    imgArray: [ "/img/work1.jpg", "/img/work2.jpg", "/img/work3.jpg"],
+    imgArray: [ "/img/20-workspace.jpg", "/img/01-workspace.jpg", "/img/artists-workspace.jpg"],
     imgNo: 0,
     imgArrayLength: 3,
     url: "https://www.deskbookers.com/nl-nl/sajax.json?q=Amsterdam&type=-&people=any&favorite=0&pid=&sw=52.293753%2C4.634942&ne=52.455562%2C5.162286&ids=17201%2C19640%2C13692%2C13691%2C12136%2C17938%2C15292%2C14886%2C14885%2C14884%2C14883%2C15730%2C15353%2C15351%2C15330%2C15080%2C17290%2C15454%2C15451%2C15379",
@@ -25,6 +25,7 @@ export default class Slider extends React.Component {
    componentDidMount(){
      this.serverRequest = $.get(this.state.url, function(result){
        var info =  result;
+       console.log(info);
        this.setState({
         resultObject:info
       })
