36e797b0a9f5ef5b275cafad859986da9609d126
islam <islamheshamashraf@gmail.com>
2016-10-25 10:39:25 +0300

    added-modal

--- src/Card.js
+++ src/Card.js
@@ -2,19 +2,52 @@ import React from 'react';
 
 export default class Card extends React.Component {
 
+  constructor(){
+    super()
+    this.state = {
+      name: null,
+      image: null,
+      price: null,
+      location: null,
+      score: null
+    };
+  }
+
+
+/**
+* opens modal to show more info.
+*/
+openModal(index){
+  var x = index;
+  var results = this.props.resultsArray;
+  this.setState({
+    name: results[x].name,
+    image: results[x].image,
+    price: results[x].price,
+    location: results[x].location,
+    score: results[x].score
+  })
+  var modal = $('.modal');
+  modal.show();
+  $(".modal-close").click(function(){
+    modal.hide();
+  });
+}
+
  render(){
   /**
    * Populates list items according to data passed
    * on to resultsArray.
    */
     var items = this.props.resultsArray;
+    var that = this;
     var itemslist = items.map(function(item, index){
           return(
-              <li key={ index } class="card" >
+              <li key={ index } id={index} class="card" >
                 <div class="card-header">
                     <span class="hour-price"><span>{ item.hourPrice } &euro; /hour</span></span>
                     <img src={ item.image } class="card-img" />
-                    <a href="javascript:" class="book-button">Book</a>
+                    <a href="javascript:"  onClick={that.openModal.bind(that, index)} class="book-button">More..</a>
                   </div>
                 <div>
                   <div class="card-info">
@@ -33,6 +66,17 @@ export default class Card extends React.Component {
         <ul class="card-list center">
           { itemslist }
          </ul>
+         <div class="modal">
+           <div class="modal-content">
+            <div class="modal-header">
+              <img class="modal-img" src={this.state.image} />
+              <img src="/img/error.png" class="modal-close" />
+            </div>
+            <div class="modal-body">
+
+            </div>
+           </div>
+         </div>
       </div>   
       );
 
--- src/Slider.js
+++ src/Slider.js
@@ -3,19 +3,19 @@ import Card from './Card.js';
 
 export default class Slider extends React.Component {
   
- constructor() {
-  super()
-  this.state = {  
-    imgArray: [ "/img/01-workspace.jpg", "/img/20-workspace.jpg", "/img/artists-workspace.jpg"],
-    imgNo: 0,
-    imgArrayLength: 3,
-    url: "https://www.deskbookers.com/nl-nl/sajax.json?q=Amsterdam&type=-&people=any&favorite=0&pid=&sw=52.293753%2C4.634942&ne=52.455562%2C5.162286&ids=17201%2C19640%2C13692%2C13691%2C12136%2C17938%2C15292%2C14886%2C14885%2C14884%2C14883%2C15730%2C15353%2C15351%2C15330%2C15080%2C17290%2C15454%2C15451%2C15379",
-    search: '',
-    resultObject: null,
-    resultsArray: [],
-    headlines: ["Pink Floyd Office", "Led Zeppelin Mania", "Central Perk Friends"]
-  };
-}
+   constructor() {
+    super()
+    this.state = {  
+      imgArray: [ "/img/01-workspace.jpg", "/img/20-workspace.jpg", "/img/artists-workspace.jpg"],
+      imgNo: 0,
+      imgArrayLength: 3,
+      url: "https://www.deskbookers.com/nl-nl/sajax.json?q=Amsterdam&type=-&people=any&favorite=0&pid=&sw=52.293753%2C4.634942&ne=52.455562%2C5.162286&ids=17201%2C19640%2C13692%2C13691%2C12136%2C17938%2C15292%2C14886%2C14885%2C14884%2C14883%2C15730%2C15353%2C15351%2C15330%2C15080%2C17290%2C15454%2C15451%2C15379",
+      search: '',
+      resultObject: null,
+      resultsArray: [],
+      headlines: ["Pink Floyd Office", "Led Zeppelin Mania", "Central Perk Friends"]
+    };
+  }
 
 
   /**
